db4a8a689edff700aeb612faa19ffbec1715c256
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2023-10-03 15:46:29 +0200

    change flake

--- flake.nix
+++ flake.nix
@@ -3,13 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # home-manager = {
-    #   url = "github:nix-community/home-manager";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: 
+  outputs = inputs@{ nixpkgs, home-manager, ... }: 
   let 
     system = "x86_64-linux";
 
@@ -22,6 +22,13 @@
         modules = [
           ./nixos/configuration.nix
           ./machines/base.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.tiggax = import ./users/tiggax.nix;
+          }
         ];
       };
       testing = nixpkgs.lib.nixosSystem {
