aa551903a8d570fed7be7860c52a189682821178
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2023-10-06 23:33:22 +0200

    add network discovribotiltxy (u know what i mean)

--- printers/Firma/default.nix
+++ printers/Firma/default.nix
@@ -1,9 +1,16 @@
 { pkgs, ...}:
 {
-services.printing = {
-  enable = true;
-  drivers = with pkgs; [
-    foomatic-db-ppds
-  ];
-};
+  services = {
+    avahi = {
+      enable = true; # runs the Avahi daemon
+      nssmdns = true; # enables the mDNS NSS plug-in
+      openFirewall = true;
+    };
+    printing = {
+      enable = true;
+      drivers = with pkgs; [
+        foomatic-db-ppds
+      ];
+    };
+  };
 }
