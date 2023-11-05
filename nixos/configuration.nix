5248a75ec9e0f99bebae662919a58db952eb7233
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2023-11-05 19:59:53 +0100

    downgrade kernel to 6.5

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -24,7 +24,7 @@
   networking.hostName = "tixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
  
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_6_5;
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
