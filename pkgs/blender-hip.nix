793384b830cd09d9dc29fbf6e879567d79b9bb6f
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2023-11-05 20:39:40 +0100

    blender doesn't seem to need these, keeping them in just in case

--- pkgs/blender-hip.nix
+++ pkgs/blender-hip.nix
@@ -1,14 +1,13 @@
 { pkgs, ... }:
 {
-  users.users.tiggax.extraGroups = [ "render" "video" ];
+  # users.users.tiggax.extraGroups = [ "render" "video" ];
 
   environment.systemPackages = with pkgs;[
     blender-hip
   ];
 
-  # boot.initrd.kernelModules = [ "amdgpu" ]; # This should maybe be better in hardware.
-  hardware.opengl.extraPackages = with pkgs; [
-    rocm-opencl-icd
-    rocm-opencl-runtime
-  ];
+  # hardware.opengl.extraPackages = with pkgs; [
+  #   rocm-opencl-icd
+  #   rocm-opencl-runtime
+  # ];
 }
