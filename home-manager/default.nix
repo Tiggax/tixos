eb244633c587d1efe5e5160023fad7d991d74e2e
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2024-06-23 00:31:28 +0200

    test

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,19 +1,22 @@
 { config, pkgs, userSettings, ... }:
 {
 
-  home.username = userSettings.username;
-  home.homeDirectory = "/home/${userSettings.username}";
+  home-manager.${userSettings.username} = {
+    home.username = userSettings.username;
+    home.homeDirectory = "/home/${userSettings.username}";
+    
+    home.packages = with pkgs; [
+      gitui
+      git
 
-  home.packages = with pkgs; [
-    gitui
-    git
+      helix
+    
+      nil
 
-    helix
-  
-    nil
+    ];
+    home.stateVersion = "23.05";
 
-  ];
-  home.stateVersion = "23.05";
+    programs.home-manager.enable = true;
 
-  programs.home-manager.enable = true;
+  };
 }
