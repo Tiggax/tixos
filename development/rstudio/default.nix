a1ea0e74bbadfb1cad67accb2c546647e3826786
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2024-10-10 20:41:18 +0200

    create rstudio module

---
+++ development/rstudio/default.nix
@@ -0,0 +1,25 @@
+{pkgs, lib, config, userSettings, home-manager, ...}:
+let
+    cfg = config.development.rstudio;
+in
+{
+    options.development.rstudio = {
+        enable = lib.mkEnableOption "Enable RStudio";
+        additionalPackages = lib.mkOption {
+            type = lib.types.listOf types.package;
+            default = [];
+            example = [pkgs.rPackages.tidyverse];
+            description = "Additional packages to install";
+        };
+    };
+
+    config = lib.mkIf cfg.enable {
+        environment.systemPackages = with pkgs; with rPackages;[
+            rstudio
+
+            # Packages
+            tidyverse
+
+        ] ++ cfg.additionalPackages;
+    };
+}
