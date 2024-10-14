0b43d410bbdc0a0d8f4ed6940bc90d7865089364
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2024-10-14 09:17:18 +0200

    comment out uniprotR cuz it cant build on machine

--- nixos/profiles/flyingTixos.nix
+++ nixos/profiles/flyingTixos.nix
@@ -13,7 +13,10 @@
     networking.hostName = "flyingTixos";
 
     development.enable = true;
-    development.rstudio.additionalPackages = with pkgs; with rPackages;[ UniprotR bio3d ];
+    development.rstudio.additionalPackages = with pkgs; with rPackages;[ 
+        #UniprotR 
+        bio3d 
+    ];
 
     programs.nix-ld = {
         enable = true;
