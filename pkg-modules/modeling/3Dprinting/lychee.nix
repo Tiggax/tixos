965d9bdf2fe84b337583d3a10e40e10f9d1549d4
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2024-11-18 22:49:04 +0100

    finilize the applet

--- pkg-modules/modeling/3Dprinting/lychee.nix
+++ pkg-modules/modeling/3Dprinting/lychee.nix
@@ -1,6 +1,6 @@
 { appimageTools, fetchurl, ... }:
 let
-  pname = "Lychee_Slicer";
+  pname = "lycheeslicer";
   version = "7.1.1";
 
   name = "${pname}-${version}";
@@ -9,22 +9,34 @@ let
     url = "https://mango-lychee.nyc3.digitaloceanspaces.com/LycheeSlicer-${version}.AppImage";
     sha256 = "sha256-3UEih6i/JkBOCDJ78bkQ3nJ8P07fZCy0a4/t6yGXChA=";
   };
-  appimageContents = appimageTools.extractType2 {inherit name src version; };
+  
+  desktopFile = ''
+    [Desktop Entry]
+    Name=Lychee Slicer
+    Comment=Lychee Slicer for 3D printing
+    Exec= $out/bin/${pname} --no-sandbox %U
+    Terminal=false
+    Type=Application
+    Icon=${pname}
+    MimeType=x-scheme-handler/lycheeslicer;model/stl;
+    Categories=Graphics;3DGraphics;
+  '';
+
+  appimageContents = appimageTools.extractType2 { inherit name src; };
 in
 appimageTools.wrapType2 {
   inherit pname version src;
 
-
   extraPkgs = pkgs: [];
 
   extraInstallCommands = ''
-    mv $out/bin/${name} $out/bin/${pname}
-    install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
-    substitueInPlace $out/share/applications/${pname}.desktop \
-      --replace-fail 'Exec=AppRun' 'Exec=$pname'
-    cp -r ${appimageContents}/usr/share/icons $out/share
+    mkdir -p $out/share/applications
+    echo "${desktopFile}" > $out/share/applications/${pname}.desktop
+
+    cp -r ${appimageContents}/usr/share/icons $out/share/icons
   '';
-  
+
+    
   meta = {
     description = "Lychee Slicer is a powerful and user-friendly slicing software created to simplify your 3D printing experience. Compatible with over 750 printers, our slicer fits the needs of both beginners and advanced users.";
     homepage = "https://lychee.mango3d.io/resin-sla-msla-3d-printers";
